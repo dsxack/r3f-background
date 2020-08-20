7edfe89090607f2f83d4cc712940ae746268586f
Smotrov Dmitriy <dsxack@gmail.com>
2020-08-20 08:39:47 +0300

    Adjust blur scale

--- src/components/Scene/Scene.tsx
+++ src/components/Scene/Scene.tsx
@@ -8,16 +8,22 @@ import {useAspect} from "../../hooks/aspect";
 
 
 const startPalette1: THREE.Color[] = map([
-    "#E2D0E6", "#E2D0E6", "#E2D0E6", "#E2D0E6", "#E2D0E6",
-    "#E3D4E8", "#E3D4E8", "#E3D4E8", "#E0D4EA", "#E5D4E9",
-    "#E4D5E9", "#E6D5EA", "#E6D5EA", "#E6D5EA", "#E6D5EA",
-    "#E8D6DF", "#E8D6E0", "#E8D6DF", "#E8D6DF", "#E8D6E0",
-    "#EED8D0", "#F0D5CE", "#EED9D1", "#EED9D1", "#EED9D1",
-    "#F5CCD1", "#FBC8C5", "#F6D0C3", "#F6D3C3", "#F5D4C0",
-    "#F8CACD", "#FCC9BD", "#F9C6B5", "#FAC7B7", "#F8D0BD",
-    "#FDC8B7", "#F8C4BA", "#F1C6BF", "#F5C5BC", "#F7C5B9",
-    "#FAC4C0", "#F9C1C1", "#EBC5CB", "#ECC7C6", "#EFC4C7",
-], (hex: string) => new THREE.Color(hex))
+    "#818181", "#A6A6A6", "#8B8B8B", "#6A6A6A", "#7B7B7B",
+    "#737373", "#8C8C8C", "#A3A3A3", "#B0B0B0", "#ADADAD",
+    "#A0A0A0", "#666666", "#ADADAD", "#9E9E9E", "#848484",
+    "#707070", "#737373", "#9C9C9C", "#6D6D6D", "#6A6A6A",
+    "#7B7B7B", "#828282", "#717171", "#575757", "#5C5C5C",
+    "#797979", "#686868", "#5B5B5B", "#616161", "#676767",
+    "#828282", "#4A4A4A", "#4D4D4D", "#686868", "#595959",
+    "#6B6B6B", "#717171", "#545454", "#343434", "#4F4F4F",
+    "#A7A7A7", "#959595", "#707070", "#5D5D5D", "#858585",
+], (hex: string) => {
+    const color = new THREE.Color(hex)
+    color.r = color.r * 1
+    color.g = color.g * 1.5
+    color.b = color.b * 2
+    return color
+})
 
 
 const startPalette2: THREE.Color[] = map([
--- src/hooks/effects.ts
+++ src/hooks/effects.ts
@@ -5,29 +5,32 @@ import * as THREE from "three";
 import {BlurPass, EffectComposer, RenderPass, KernelSize} from "postprocessing";
 
 export const useEffects = () => {
-    const { gl, scene, camera, size } = useThree()
+    const {gl, scene, camera, size} = useThree()
 
     const blurPass = useMemo(() => {
         const blurPass = new BlurPass({
-            kernelSize: KernelSize.VERY_LARGE,
+            kernelSize: KernelSize.HUGE,
         })
         blurPass.renderToScreen = true
-        blurPass.scale = 1
-
         return blurPass
     }, [])
+    useEffect(() => {
+        blurPass.scale = 1.5 + size.width / 2000
+        blurPass.setSize(size.width, size.height)
+    }, [blurPass, size])
 
     const composer = useMemo(() => {
-        const composer = new EffectComposer(gl, { frameBufferType: THREE.HalfFloatType })
+        const composer = new EffectComposer(gl, {frameBufferType: THREE.HalfFloatType})
 
         composer.addPass(new RenderPass(scene, camera))
         composer.addPass(blurPass)
 
         return composer
     }, [blurPass, camera, gl, scene])
+    useEffect(() => {
+        composer.setSize(size.width, size.height)
+    }, [composer, size])
 
-    useEffect(() => void composer.setSize(size.width, size.height), [composer, size])
-    useEffect(() => void blurPass.setSize(size.width, size.height), [blurPass, size])
 
     return useFrame((_, delta) => composer.render(delta), 1)
 }
